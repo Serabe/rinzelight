f3b9b6a7eed18f3bc44ff140b4b41c5ddf98bd1e
Sergio Arbeo <serabe@gmail.com>
2010-09-08 00:17:35 +0200

    Updated project.clj to Clojure and Clojure-contrib 1.2

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject rinzelight "0.0.1"
   :description "Magick-like image proccessing for Clojure"
-  :dependencies [[org.clojure/clojure "1.1.0"] [org.clojure/clojure-contrib "1.1.0-master-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
                      [swank-clojure "1.2.1"]])
 
