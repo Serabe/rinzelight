a1bd4fe993b5cb40e562ccc0fcf021a7cffa15f4
Sergio Arbeo <serabe@gmail.com>
2010-09-12 22:54:00 +0200

    Basic testing.

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[lein-clojars "0.5.0"]
-                     [swank-clojure "1.2.1"]])
+                     [swank-clojure "1.2.1"]
+                     [midje "0.4.0"]])
