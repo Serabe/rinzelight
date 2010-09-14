d7125ec6e87e3ec2dfee4d5e829c7d1513ca5227
Sergio Arbeo <serabe@gmail.com>
2010-09-14 22:31:41 +0200

    Added criterium to project.clj

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[lein-clojars "0.5.0"]
                      [swank-clojure "1.2.1"]
-                     [midje "0.4.0"]])
+                     [midje "0.4.0"]
+	             [criterium "0.0.1-SNAPSHOT"]])
