3030e8ff707578eb1b766ccda523e076de784992
Sergio Arbeo <serabe@gmail.com>
2010-09-26 22:59:06 +0200

    Added lein-run to dev deps

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
   :dev-dependencies [[lein-clojars "0.5.0"]
                      [swank-clojure "1.2.1"]
                      [midje "0.4.0"]
+                     [lein-run "1.0.0"]
 	             [criterium "0.0.1-SNAPSHOT"]])
