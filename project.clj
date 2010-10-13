7a81d04e30913f47197c3d2638cf27c1312f4e86
Sergio <sergio@tyrion.(none)>
2010-10-13 23:25:04 +0200

    Modified project.clj for clojure 1.3.0 alpha

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject org.clojars.serabe/rinzelight "0.0.22"
   :description "Magick-like image proccessing for Clojure"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.3.0-alpha1"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[lein-clojars "0.5.0"]
                      [swank-clojure "1.2.1"]
