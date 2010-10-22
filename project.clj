fabc74876257053de9178ce56a5d91dc353cc747
Sergio Arbeo <serabe@serabe.com>
2010-10-22 23:04:35 +0200

    Updated midje to 0.6.1

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :dependencies [[org.clojure/clojure "1.2.0"]]
   :dev-dependencies [[lein-clojars "0.6.0"]
                      [swank-clojure "1.2.1"]
-                     [midje "0.4.0"]
+                     [midje "0.6.1"]
                      [lein-run "1.0.0"]
 	             [criterium "0.0.1-SNAPSHOT"]])
  
