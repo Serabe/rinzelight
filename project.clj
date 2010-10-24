7f63a8c056dd939df3f10782bfb06cd6021b33cc
Sergio Arbeo <serabe@serabe.com>
2010-10-24 21:10:09 +0200

    Added autodoc to rinzelight.
    Currently, an IllegalArgumentException
    is raised if autodoc leiningen task is
    used. I trust it is because I am running
    it from a pen drive.

--- project.clj
+++ project.clj
@@ -1,9 +1,19 @@
 (defproject org.clojars.serabe/rinzelight "0.0.2"
   :description "Image proccessing for Clojure"
-  :dependencies [[org.clojure/clojure "1.2.0"]]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[lein-clojars "0.6.0"]
                      [swank-clojure "1.2.1"]
                      [midje "0.6.1"]
                      [lein-run "1.0.0"]
-	             [criterium "0.0.1-SNAPSHOT"]])
+	             [criterium "0.0.1-SNAPSHOT"]
+		     [autodoc "0.7.1"]]
+  :autodoc {
+            :name "Rinzelight"
+            :description "A beautiful image processing library for Clojure."
+            :web-src-dir "http://github.com/Serabe/rinzelight/blob"
+            :web-home "http://serabe.github.com/rinzelight"
+            :trim-prefix "rinzelight"
+            :page-title "Rinzelight image library"
+            :copyright "Copyright 2010 Sergio Arbeo. All rights reserved."})
  
