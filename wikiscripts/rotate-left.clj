c3706af3bb84037825bada61d6f4118fd7eebfcf
Anonymous <Anonymous@Everywhere>
2010-10-25 19:04:47 +0200

    Updated rotate-left wikiscript

--- wikiscripts/rotate-left.clj
+++ wikiscripts/rotate-left.clj
@@ -5,9 +5,8 @@
 
 (def img (read-image "samples/northern-lights.jpg"))
 
-(write-image
- (map-pixel-location (rotate-left img)
-                     img
-                     ((rotate-left-new-size img) 0)
-                     ((rotate-left-new-size img) 1))
- "out/mpl-rotate-left.png")
+(write-image (map-pixel-location (rotate-left img)
+                                 rotate-left-new-size
+                                 img)
+             "out/mpl-rotate-left.png")
+
