37928321cbb43f9a38b597860aa1bf41d9951eaa
Anonymous <Anonymous@Everywhere>
2010-10-25 10:44:48 +0200

    Added a script for creating examples for the wiki

---
+++ wikiscripts/zero.clj
@@ -0,0 +1,23 @@
+(ns examples.only-green-lookup-tables
+  (:use rinzelight.image
+        rinzelight.effects.lookup-tables
+        rinzelight.rendering-hints))
+
+(def img (read-image "samples/northern-lights.jpg"))
+
+(defmacro lt-example
+  [img name]
+  (let [out-file (str "out/lt-" name ".png")]
+    `(write-image (apply-lookup-table ~img
+                                      (multisample-lookup-table ~name)
+                                      color-rendering-quality
+                                      antialiasing-on
+                                      rendering-quality)
+                  ~out-file)))
+
+(lt-example img zero)
+(lt-example img straight)
+(lt-example img invert)
+(lt-example img brighten)
+(lt-example img better-brighten)
+(lt-example img posterize)
