d5fc2e5d6f02ff8dbe52ee21387eb75e39e7786f
Anonymous <Anonymous@Everywhere>
2010-10-25 19:07:33 +0200

    Added cropping.

---
+++ src/rinzelight/crop.clj
@@ -0,0 +1,23 @@
+(ns rinzelight.crop
+  (:use [rinzelight.image
+         :only [create-image]]
+        [rinzelight.composition
+         :only [src-over
+                with-composite]])
+  (:import (java.awt AlphaComposite)))
+
+(defn crop
+  "Crops an image to a width of w, height of h, starting on pixel x,y.
+If a geometry is passed, it is used to calculate the x and y."
+  ([img w h]
+     (crop img w h 0 0))
+  ([img w h geom]
+     (let [[x y] (geom (:width img) (:height img)
+                       w h)]
+       (crop img w h x y)))
+  ([img w h x y]
+     (let [ni (create-image w h)
+           si (create-image (.getSubimage (:image img) x y
+                                          (min w (- (:width img) x))
+                                          (min h (- (:height img) y))))]
+       (compose ni si (src-over)))))
