db9b8c8a49574c3a569251a32fed51302125670d
Anonymous <Anonymous@Everywhere>
2010-10-25 10:45:46 +0200

    Modified alpha constants in order to reflect that ImageMagick scale is no longer valid for alpha channel.

--- src/rinzelight/constants.clj
+++ src/rinzelight/constants.clj
@@ -12,7 +12,7 @@
   [name value]
   `(defmacro ~name [] (list double ~value)))
 
-(rinze-int-constant opaque-opacity 0)
 (rinze-int-constant quantum-range 255)
-(rinze-int-constant transparent-opacity (quantum-range))
+(rinze-int-constant opaque-opacity 255)
+(rinze-int-constant transparent-opacity 0)
 (rinze-double-constant quantum-scale (/ 1.0 (quantum-range)))
