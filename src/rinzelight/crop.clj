d78442155318f6a7bbf45c068d2aa6c601145dfe
Sergio Arbeo <serabe@serabe.com>
2010-10-26 11:57:14 +0200

    Chorradaca.

--- src/rinzelight/crop.clj
+++ src/rinzelight/crop.clj
@@ -19,6 +19,6 @@ If a gravity is passed, it is used to calculate the x and y."
      (let [ni (create-image w h) ; just in case the crop exceed the
                                  ; image boundaries
            si (create-image (.getSubimage (:image img) x y
-                                          (min w (- (:width img) x))
+                                          (min w (- (:width  img) x))
                                           (min h (- (:height img) y))))]
        (compose ni si (src-over)))))
