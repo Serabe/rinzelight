63326c7432ae5f9868529a34394122adfb61ec7a
Sergio Arbeo <serabe@gmail.com>
2012-01-13 02:27:54 +0100

    Adds constant kernel-rank and function rinze-sigma.
    These two additions are needed for blur effect.

--- src/rinzelight/constants.clj
+++ src/rinzelight/constants.clj
@@ -12,9 +12,12 @@
   [name value]
   `(defmacro ~name [] (list double ~value)))
 
+
+(rinze-int-constant kernel-rank 3)
 (rinze-int-constant quantum-range 255)
 (rinze-int-constant opaque-opacity 255)
 (rinze-int-constant transparent-opacity 0)
 (rinze-double-constant quantum-scale (/ 1.0 (quantum-range)))
 (rinze-double-constant epsilon 1.0e-10)
 (rinze-double-constant sq2pi 2.50662827463100024161235523934010416269302368164062)
+(def rinze-sigma (partial max (epsilon)))
