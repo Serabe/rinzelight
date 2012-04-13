fd8bd13eb87cd7e0ddf455dd306b766b50826b4f
Sergio Arbeo <serabe@gmail.com>
2012-04-13 17:45:19 +0200

    New version with blur.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.serabe/rinzelight "0.0.3"
+(defproject org.clojars.serabe/rinzelight "0.0.4"
   :description "Image proccessing for Clojure"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
